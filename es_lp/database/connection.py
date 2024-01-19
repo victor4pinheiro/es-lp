ebff32af451a3a88c84134fa3de37572af3eb599
victor4pinheiro <victor.pinheiro.soft@gmail.com>
2024-01-19 14:05:56 -0300

    refactor: move connection to its file

---
+++ es_lp/database/connection.py
@@ -0,0 +1,8 @@
+from utils import config
+from elasticsearch import Elasticsearch
+
+client = Elasticsearch(
+    "https://localhost:9200/",
+    api_key=config.ELASTIC_API,
+    ca_certs=config.ELASTIC_CA_CERTS,
+)
