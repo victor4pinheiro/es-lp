02b42c385b5dc6f30123606c8139ec816c00c1e6
victor4pinheiro <victor.pinheiro.soft@gmail.com>
2024-01-19 14:08:16 -0300

    refactor: divide the application into its module

--- es_lp/__init__.py
+++ es_lp/__init__.py
@@ -1,19 +1,3 @@
-from os import getenv
-from elasticsearch import Elasticsearch
-from dotenv import load_dotenv
+from sync import sync
 
-load_dotenv()
-
-ELASTIC_API = getenv("ES_API")
-ELASTIC_CA_CERTS = getenv("CA_CERTS") or "http_ca.crt"
-ELASTIC_USER = getenv("ELASTIC_USER") or "elastic"
-ELASTIC_PASSWORD = getenv("ELASTIC_PASSWORD") or "SuperSecret"
-
-client = Elasticsearch(
-    "https://localhost:9200/",
-    api_key=ELASTIC_API,
-    ca_certs=ELASTIC_CA_CERTS,
-    basic_auth=(ELASTIC_USER, ELASTIC_PASSWORD),
-)
-
-client.indices.create(index="my_index")
+sync()
