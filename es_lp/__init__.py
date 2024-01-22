2e7607ef4b6c84e01cdd012f14e84ea5200deeb6
victor4pinheiro <victor.pinheiro.soft@gmail.com>
2024-01-22 14:17:20 -0300

    refactor: move some responsabilities to anothers components

--- es_lp/__init__.py
+++ es_lp/__init__.py
@@ -1,3 +1,12 @@
-from sync import sync
+from flask import Flask
+from flask_marshmallow import Marshmallow
+from es_lp.routes import routes
 
-sync()
+
+app = Flask(__name__)
+app.register_blueprint(routes)
+ma = Marshmallow(app)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
