e0de3deaf8079bcc0afe542cb81a43244255aed4
victor4pinheiro <victor.pinheiro.soft@gmail.com>
2024-01-23 14:30:24 -0300

    feat: add another route

--- es_lp/routes.py
+++ es_lp/routes.py
@@ -1,5 +1,7 @@
 from flask import Blueprint
 from es_lp.controller.pokemon_controller import pokemon_bp
+from es_lp.sync import pokeapi_bp
 
 routes = Blueprint('route', __name__, url_prefix='/api')
 routes.register_blueprint(pokemon_bp)
+routes.register_blueprint(pokeapi_bp)
