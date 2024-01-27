5de6eca12ffbf04787078165e9b87a7d31cee51d
victor4pinheiro <victor.pinheiro.soft@gmail.com>
2024-01-26 22:14:44 -0300

    feat: add a custom search with query params

--- es_lp/controller/pokemon_controller.py
+++ es_lp/controller/pokemon_controller.py
@@ -1,5 +1,6 @@
 from logging import error
 from flask import Blueprint, Response, request
+from elasticsearch_dsl import Search, Q
 from es_lp.database.connection import client as elastic_search
 from es_lp.middleware.messages import format_messages
 from es_lp.models.pokemon import PokemonSchema
@@ -51,28 +52,55 @@ def list_all_pokemons() -> Response:
 
     return response
 
-
 @pokemon_bp.get("/search")
-def custom_search_pokemon():
-    pokemons_schema = PokemonSchema(many=True)
-    queries_params = request.args
+def search_pokemons():
+    dynamic_queries = request.args.to_dict()
+    size = request.args.get("size", 10000, type=int)
+
+    if "size" in dynamic_queries:
+        del dynamic_queries["size"]
 
     try:
-        custom_search_response = elastic_search.search(
-            index="pokemon",
-            size=int(queries_params.get("limit") or 10000),
-            query={"match": {"name": queries_params.get("name")}},
+        query = build_dynamic_query(dynamic_queries)
+        s = (
+            Search(using=elastic_search, index="pokemon")
+            .query(query)
+            .source(["_id", "name"])
+            .extra(size=size)
         )
-
-        pokemons = custom_search_response["hits"]["hits"]
-        output = pokemons_schema.dump(
-            [
-                {"_id": pokemon["_id"], "name": pokemon["_source"]["name"]}
-                for pokemon in pokemons
-            ]
-        )
-        response = format_messages({"pokemons": output}, 200)
+        response = s.execute()
     except Exception as e:
-        response = format_messages(f"Error: {str(e)}", 500)
+        return format_messages(f"Error: {str(e)}", 400)
 
-    return response
+    results = []
+    for hit in response:
+        results.append(
+            {
+                "_id": hit.meta.id,
+                "name": hit.name,
+            }
+        )
+
+    return format_messages({"pokemons": results}, 200)
+
+
+def build_dynamic_query(query_params):
+    query = Q()
+    tmp_match = None
+
+    try:
+        for field, value in query_params.items():
+            if field == "match":
+                if field == "all" or field == "match":
+                    tmp_match = None
+                else:
+                    tmp_match = value
+            else:
+                if tmp_match:
+                    query &= Q(f"match_{tmp_match}", **{field: value})
+                else:
+                    query &= Q("match", **{field: value})
+    except Exception as e:
+        return format_messages(f"Error: {str(e)}", 404)
+
+    return query
